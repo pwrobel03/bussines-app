019a161cf2ba8218fa7202cbf61dcfdf76a12ddd
Piotr Wrobel <wrobelpiotr24@gmail.com>
2023-10-29 18:17:34 +0100

    businnes progress

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -5,9 +5,26 @@ import Button from './Button'
 
 const Business = () => {
   return (
-    <section id='features' className='flex md:flex-row flex-col sm:puy-16 py-6'>
-      <div className='flex flex-1 flex-col justify-center items-center'>
-        <h2>You do the business, we'll handle the money</h2>
+    <section id='features' className='flex flex-1 md:flex-row flex-col sm:puy-16 py-6'>
+      <div className='flex flex-col justify-center items-start'>
+        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden'></br>we'll handle the money</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <Button styles='mt-10'></Button>
+      </div>
+      <div className='flex-1 flex flex-col'>
+        {features.map((feature) => {
+          return (
+            <div key={feature.id} className='flex sm:my-6 py-1 justify-center items-center flex-row rounded-[20px] hover:bg-discount-gradient'>
+              <div className='flex flex-1 w-[64px] h-[64px] items-start object-contain bg-dim-blue'>
+                <img src={feature.icon} alt={feature.title} />
+              </div>
+              <div className='flex flex-col items-start justify-start'>
+                <div className='flex'>{feature.title}</div>
+                <div className='flex'>{feature.content}</div>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </section>
   )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
     return (
-        <div>
-            asdasd
-        </div>
+        <button type="button" className={`py-4 px-6 bg-blue-gradient capitalize font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+            get started
+        </button>
     )
 }
 
