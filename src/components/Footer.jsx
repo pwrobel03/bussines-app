66300ffe09346651f96680fb32b89675a84053bb
Piotr Wrobel <wrobelpiotr24@gmail.com>
2023-10-31 21:43:44 +0100

    footer-main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,22 @@ import { footerLinks } from '../constants'
 const Footer = () => {
     return (
         <footer className='flex sm:py-16 py-6 flex-1 flex-col'>
-            <div className='flex flex-1 md:flex-row flex-col'>
-                <div className='flex flex-1 flex-col'>
+            <div className='flex flex-1 gap-8 md:flex-row flex-col'>
+                <div className='flex flex-col md:w-1/3 w-[100%]'>
                     <img src={logo} alt="Hoo Bank" className="w-[300px]" />
                     <p className={styles.paragraph}>
                         A new way to make the payments easy, reliable and secure.
                     </p>
                 </div>
-                <div className='flex flex-1'>
+                <div className='flex md:my-0 my-8 sm:flex-row flex-col justify-between md:w-2/3 w-[100%]'>
                     {footerLinks.map(footerLink => (
-                        <div key={footerLink.title} className="flex flex-1 flex-col">
-                            <h4 className='text-white text-[20px] leading-[28px]'>{footerLink.title}</h4>
-                            <ul className='text-white'>
-                                {footerLink.links.map(link => (
-                                    <li className='hover:text-secondary cursor-pointer'>{link.name}</li>
+                        <div key={footerLink.title} className="flex sm:mb-0 mb-6 flex-col sm:items-start items-center">
+                            <h4 className='text-white text-[24px] leading-[32px] mb-2'>{footerLink.title}</h4>
+                            <ul className='text-white sm:text-start text-center'>
+                                {footerLink.links.map((link, index) => (
+                                    <li key={index} className={`${styles.paragraph} hover:text-secondary cursor-pointer text-gray`}>
+                                        <a href={link.link} target="_blank">{link.name}</a>
+                                    </li>
                                 ))}
                             </ul>
                         </div>
