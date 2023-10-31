d790b61cdeb7a465fda337a2f5da16a57c4dbd7f
Piotr Wrobel <wrobelpiotr24@gmail.com>
2023-10-31 22:00:48 +0100

    toogle-menu-fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false)
 
     return (
-        <nav className="w-full flex py-6 justify-between items-center navbar">
+        <nav className="w-full flex py-[32px] justify-between items-center navbar">
             <img src={logo} alt="logo" className='w-[124px] h-[32px]' />
             <ul className="list-none sm:flex hidden justify-end items-center flex-1">
                 {navLinks.map((nav, index) => (
@@ -25,12 +25,13 @@ const Navbar = () => {
                     className='w-[28px] h-[28px] object-contain'
                     onClick={() => setToggle((prev) => !prev)} />
 
-                <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] rounded-xl sidebar`}>
+                <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-primary absolute top-20 right-0 w-[100%] z-[6] sidebar`}>
                     <ul className="list-none flex justify-end items-center flex-1 flex-col">
                         {navLinks.map((nav, index) => (
                             <li
+                                onClick={() => setToggle((prev) => !prev)}
                                 key={nav.id}
-                                className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
+                                className={`font-poppins font-normal py-4 hover:text-secondary cursor-pointer text-[18px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
                                 <a href={`#${nav.id}`}>{nav.title}</a>
                             </li>
                         ))}
